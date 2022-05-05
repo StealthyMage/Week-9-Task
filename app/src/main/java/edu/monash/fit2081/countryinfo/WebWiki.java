272660beccf692c6f44590969684d3ec34ff1a6c
StealthyMage <36330911+StealthyMage@users.noreply.github.com>
2022-05-05 10:44:02 +1000

    All tasks done!

--- app/src/main/java/edu/monash/fit2081/countryinfo/WebWiki.java
+++ app/src/main/java/edu/monash/fit2081/countryinfo/WebWiki.java
@@ -4,18 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebWiki extends AppCompatActivity {
     WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myWebView = new WebView(this);
         setContentView(R.layout.activity_web_wiki);
         Intent intent = getIntent();
         Bundle args = intent.getBundleExtra("BUNDLE");
         String string = args.getString("COUNTRYNAME");
+        getSupportActionBar().setTitle("Country Wikipedia");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        myWebView = findViewById(R.id.web_view);
+        myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("https://en.wikipedia.org/wiki/" + string);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+        }
+        return true;
     }
 }
